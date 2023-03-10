cd105d9752f94ec317cc4c453e6ee10fa891ed73
Sitnikova Yekaterina <sitkat03@mail.ru>
2023-03-10 13:17:17 +0300

    Add checker for reading.

--- lib/data_controller.dart
+++ lib/data_controller.dart
@@ -1,30 +1,59 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:flutter/services.dart' show rootBundle;
 
 class DataController {
-  late final Directory _appDirectory;
+  String? _edition;
+  String _title = "";
+  String _translation = "";
+  String _description = "";
 
   void converter() async {
     WidgetsFlutterBinding.ensureInitialized();
-    File file = File(_loadFile().toString());
-    // print(file);
+    // File file = File(readFileAsync('assets/test.txt').toString());
+    readFileAsync('assets/test.txt').toString();
   }
 
   Future<dynamic> readFileAsync(String filePath) async {
-    String textString = await rootBundle.loadString(filePath);
-    // print(textString);
-    // LineSplitter.split(textString).forEach((line) => print('$line'));
-    return LineSplitter.split(textString).forEach((line) => print('$line'));
-    // return textString;
-  }
-  Future<dynamic> _loadFile() async {
-    String data = await rootBundle.loadString('assets/test.txt');
-    LineSplitter.split(data).forEach((line) {
-      print('$line');
+    String data = await rootBundle.loadString(filePath);
+    return LineSplitter.split(data).forEach((line) {
+      if (isDate(line)) {
+        _edition = line.characters.take(16).string;
+        print('$_edition');
+      } else if (line.startsWith('[')) {
+        int firstMatch = line.indexOf('[');
+        int secondMatch = line.indexOf(']');
+
+        if (_title.contains("")) {
+          _title = line.substring(firstMatch + 1, secondMatch).trim();
+        } else {
+          _title = _title.trim() +
+              " " +
+              line.substring(firstMatch + 1, secondMatch).trim();
+        }
+        print('$_title');
+
+        _translation = line.substring(secondMatch + 1).trim();
+        print('$_translation');
+        // if(line.)
+      } else if (line.isEmpty) {
+        _edition = "";
+        _title = "";
+        _translation = "";
+        _description = "";
+        print('');
+      }
+      // print('$line');
     });
+  }
+
+  bool isDate(String str) {
+    try {
+      DateTime.parse(str.characters.take(16).string);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
